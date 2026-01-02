bad5d81012b075725fc1ae89265d9e44b986177c
ImJiyun <jiyunk920@gmail.com>
2026-01-02 09:29:37 +0900

    Week01 김지윤

---
+++ week01/김지윤/1-5.sql
@@ -0,0 +1,17 @@
+SELECT
+    CAR_ID
+FROM 
+    CAR_RENTAL_COMPANY_CAR 
+WHERE
+    CAR_TYPE = "세단"
+    AND
+    CAR_ID IN (
+        SELECT
+            CAR_ID
+        FROM
+            CAR_RENTAL_COMPANY_RENTAL_HISTORY
+        WHERE
+            EXTRACT(MONTH FROM START_DATE) = 10
+    )
+ORDER BY
+    CAR_ID DESC 

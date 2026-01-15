61dbca42e0487c5bf54fc59a3b341253babd8503
bringvotrevin <bringvotrevin@gmail.com>
2026-01-15 21:08:55 +0900

    week03-1 4번 문제 수정

--- week03/임다솜/3-4.sql
+++ week03/임다솜/3-4.sql
@@ -1,1 +1,11 @@
 -- https://leetcode.com/problems/consecutive-numbers/?envType=problem-list-v2&envId=database
+
+SELECT
+    b.c AS ConsecutiveNums
+FROM (SELECT
+    IF(IF(LEAD(num) OVER (ORDER BY id) = num, num, NULL) = LAG(num) OVER (ORDER BY id), num, NULL) AS c
+FROM Logs
+) AS b
+WHERE b.c IS NOT NULL
+GROUP BY
+    b.c

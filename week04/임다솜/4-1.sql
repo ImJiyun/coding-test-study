fa71d0d41d1b02412ba60d27d5303eb58c0619c0
bringvotrevin <bringvotrevin@gmail.com>
2026-01-20 19:42:29 +0900

    week04-01 임다솜

---
+++ week04/임다솜/4-1.sql
@@ -0,0 +1,10 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/299307
+
+SELECT
+    ID,
+    CASE
+        WHEN SIZE_OF_COLONY > 1000 THEN 'HIGH'
+        WHEN SIZE_OF_COLONY > 100 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END AS SIZE
+FROM ECOLI_DATA

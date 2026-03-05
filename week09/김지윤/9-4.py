9a80075140af7dcbe3c409dc753fdb64424a3e25
ImJiyun <jiyunk920@gmail.com>
2026-03-05 16:49:59 +0900

    week09-01 김지윤

--- week09/김지윤/9-4.py
+++ week09/김지윤/9-4.py
@@ -7,3 +7,12 @@ def solution(num_list):
         for num in num_list:
             answer *= num
         return answer
+    
+# 더 깔끔한 버전
+import math
+
+def solution2(num_list):
+    if len(num_list) >= 11:
+        return sum(num_list)
+    else:
+        return math.prod(num_list)

1714ec11568a0df19922e92019c3c8ca1b7100ea
ImJiyun <jiyunk920@gmail.com>
2026-03-06 19:26:44 +0900

    week09-02 김지윤

---
+++ week09/김지윤/9-11.sql
@@ -0,0 +1,16 @@
+# 틀린 코드
+SELECT
+    d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+FROM
+    DEVELOPERS AS d
+LEFT JOIN
+    SKILLCODES AS s
+ON
+    d.SKILL_CODE & s.CODE = s.CODE
+WHERE
+    CATEGORY = 'Front End'
+ORDER BY
+    ID

33160d2a9a0199c09421b11f06fcb91ffc88f0e7
bringvotrevin <bringvotrevin@gmail.com>
2026-03-12 14:39:00 +0900

    week10-02 오답 수정 임다솜

--- week10/임다솜/10-13.sql
+++ week10/임다솜/10-13.sql
@@ -1,35 +1,32 @@
 -- https://leetcode.com/problems/seasonal-sales-analysis/
 
-# Write your MySQL query statement below
+
+
 WITH base AS (
 SELECT 
-    product_id,
     CASE
-        WHEN MONTH(sale_date) = 12 OR MONTH(sale_date) = 1 OR MONTH(sale_date) = 2 THEN 'Winter'
-        WHEN MONTH(sale_date) = 3 OR MONTH(sale_date) = 4 OR MONTH(sale_date) = 5 THEN 'Spring'
-        WHEN MONTH(sale_date) = 6 OR MONTH(sale_date) = 7 OR MONTH(sale_date) = 8 THEN 'Summer'
+        WHEN MONTH(sale_date) IN (12, 1, 2) THEN 'Winter'
+        WHEN MONTH(sale_date) IN (3,4,5) THEN 'Spring'
+        WHEN MONTH(sale_date) IN (6,7,8) THEN 'Summer'
         ELSE 'Fall'
     END AS season,
-    quantity,
-    price
-FROM sales
-), b2 AS (
-SELECT
-    season,
     category,
     SUM(quantity) AS total_quantity,
-    SUM(quantity * price) AS total_revenue,
-    ROW_NUMBER() OVER (PARTITION BY season, category) AS rn
-FROM base AS b
+    SUM(price * quantity) AS total_revenue
+FROM sales AS s
 LEFT JOIN products AS p
 USING (product_id)
 GROUP BY
     season,
     category
-ORDER BY
+), b2 AS (
+SELECT
     season,
     category,
+    ROW_NUMBER() OVER (PARTITION BY season ORDER BY total_quantity DESC, total_revenue DESC, category ASC) AS rn,
+    total_quantity,
     total_revenue
+FROM base
 )
 
 SELECT
@@ -39,3 +36,4 @@ SELECT
     total_revenue
 FROM b2
 WHERE rn = 1
+
